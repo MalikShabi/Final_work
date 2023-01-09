4d91a3b89c785282e79afcacc0d875330afd84b5
Malik Shabi <116983375+MalikShabi@users.noreply.github.com>
2023-01-09 18:56:02 +0300

    Add files via upload

---
+++ FinalWork/Program.cs
@@ -0,0 +1,62 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.WriteLine("Создадим массив из нескольких значений. Какое колличество значений будем вводить?");
+int countArrayValue = int.Parse(Console.ReadLine());
+
+string[] startArray = new string[countArrayValue];
+
+string[] FillArray(string[] array)
+{
+    for (int i = 0; i < countArrayValue; i++)
+    {
+        Console.Write($"Введите {i + 1} значение: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] SortArray(string[] array)
+{
+    int countNecessaryValue = 0;
+    int limitSymbol = 3;
+    string arrayValue = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayValue = array[i];
+        if (arrayValue.Length <= limitSymbol) countNecessaryValue++;
+    }
+
+    string[] endArray = new string[countNecessaryValue];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayValue = array[i];
+        if (arrayValue.Length <= limitSymbol)
+        {
+            endArray[index] = array[i];
+            index++;
+        }
+    }
+    return endArray;
+}
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0) Console.WriteLine("Введенные числа больше 3 символов! Массив пустой.");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"  [{array[i]}]");
+    }
+}
+
+FillArray(startArray);
+
+string[] endArray = SortArray(startArray);
+
+Console.WriteLine("\n Первоначальный массив:");
+PrintArray(startArray);
+
+Console.WriteLine("\n Массив значений с длинной (<) либо = (3) символа:");
+PrintArray(endArray);
